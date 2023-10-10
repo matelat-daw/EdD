64491c1a01697cef50da0539ce5db6d3a44cdc44
matelat-daw <cesarmatelat@gmail.com>
2023-10-11 00:29:20 +0100

    Update Html - 2

---
+++ HTML-Javascript/js/script.js
@@ -0,0 +1,6 @@
+function Show()
+{
+    let name = document.getElementById("username");
+    let here = document.getElementById("here");
+    here.innerHTML = "Hola " + name.value + " Sin Duda Serás un Gran Programador(o^o)!";
+}
