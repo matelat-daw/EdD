be485be4f1cea31fcf59b2d3741b392f665b7bf1
matelat-daw <cesarmatelat@gmail.com>
2023-10-11 16:28:32 +0100

    Update Now - 1

--- Go/main.go
+++ Go/main.go
@@ -6,4 +6,4 @@ func main() {
     print("Voy a Convertirme en un Gran Programador(o_o)!\n") // print Muestra en Panatlla el Mensaje que Está Entre Parentesis y Entre Comillas, \n es un Salto de Línea.
 }
 
-// Para Compilar el Código de Go es: go build -ldflags "-s -w" main.go
+// Para Compilar el Código de Go es: go build main.go - Para que el Programa se Pueda Ejecutar en Otros Ordenadores con el Mismo Sistema Operativo, o, go build -ldflags "-s -w" main.go - Que no será Portable. Se Ejecuta con: ./main
